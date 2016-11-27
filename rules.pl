17d1ae94282521920a725c052373db77558afb59
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-11-27 23:38:44 +0700

    receive input via engine.pl

--- rules.pl
+++ rules.pl
@@ -18,6 +18,5 @@ get_by_factors(X, Factor):-
 get_by_shapes(X, Shape):-
   ((Shape == none) -> true; shapes:shape(Shape, X)).
 
-get_by_stages(X):-
-  inputs:stage(Stage),
+get_by_stages(X, Stage):-
   ((Stage == none) -> true; growth_stage:stage(Stage, X)).
