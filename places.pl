10436e561dd3eaf198ff1eb9e56ab58861a88ca4
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-11-27 23:48:26 +0700

    fix typo

--- places.pl
+++ places.pl
@@ -11,7 +11,7 @@ place(leaf, brown_spot).
 place(leaf_sheath, brown_spot).
 place(panicle, brown_spot).
 place(glume, brown_spot).
-place(spikeket, brown_spot).
+place(spikelet, brown_spot).
 place(seed, brown_spot).
 place(leaf_sheath, sheath_blight).
 place(red_stripe, leaf).
